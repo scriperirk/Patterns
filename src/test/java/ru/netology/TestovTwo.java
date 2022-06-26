192299267ac77a525eccfacb1313ff17b92befd0
Scriper.irk <scriper.irk@gmail.com>
2022-06-26 22:20:28 +0800

    Test 8_Добавление второй задачи

--- src/test/java/ru/netology/TestovTwo.java
+++ src/test/java/ru/netology/TestovTwo.java
@@ -16,8 +16,8 @@ public class TestovTwo {
         open("http://localhost:9999");
 
         var user = generateUser("active");
-        $("[data-test-id='login'] input").setValue(user.getLogin());
-        $("[data-test-id='password'] input").setValue(user.getPassword());
+        $("[data-test-id='login'] input").setValue("Scriper");
+        $("[data-test-id='password'] input").setValue("123123");
         $("[data-test-id='action-login']").click();
         $("[id='root']").shouldHave(text("Личный кабинет"), Duration.ofSeconds(15));
     }
